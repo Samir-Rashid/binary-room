f97fe801558ce1c98712661ea45203d2142c9131
Anthony Tarbinian <atar137h@gmail.com>
2025-03-19 22:16:31 -0700

    organize add test

--- tests/add/add.arm.s
+++ tests/add/add.arm.s
@@ -2,16 +2,12 @@
 .text
 
 .global _start
-.global _main
 
-.balign 4
 _start:
 bl main
 mov x8, #93
 svc #0
 
-.balign 4
-_main:
 main:
 
 sub sp, sp, 32
---
+++ tests/tests.rs
@@ -0,0 +1,3 @@
+
+#[path = "add/test_add.rs"]
+mod add;
