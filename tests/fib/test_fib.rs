42cd146f4154a86fd21f9a14eefa135415ed7391
Anthony Tarbinian <atar137h@gmail.com>
2025-03-21 17:33:41 -0700

    fix ci

--- tests/fib/test_fib.rs
+++ tests/fib/test_fib.rs
@@ -2,13 +2,13 @@
 mod tests {
     use binary_room::instruction::*;
     use binary_room::utils::translate_to_file;
-    use binary_room::utils::START;
+    use binary_room::utils::ARM_START;
 
     #[test]
     fn test_binary_translate() {
         let riscv_asm: Vec<RiscVInstruction> = vec![
             RiscVInstruction::Verbatim {
-                text: START.to_string(),
+                text: ARM_START.to_string(),
             },
             RiscVInstruction::Addi {
                 dest: RiscVRegister::SP,
